07282cf2d182358ca976f087cf404cbfdd55249e
Didier Posse <didierposse@gmail.com>
2025-09-07 18:32:19 -0500

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -43,7 +43,7 @@ with col1:
     st.markdown("<div style='text-align: center; margin-top: 15px;'>", unsafe_allow_html=True)
     video_placeholder = st.empty()
     if st.button("🎬 Iniciar Video", use_container_width=True):
-        video_placeholder.video("https://youtu.be/d_CGX7h5IJY?si=LE1McIwzh3jZoNw8")
+        video_placeholder.video("https://youtu.be/EB7G3fUUaeA?si=HLmjqXmob46hCTOH")
     st.markdown("</div>", unsafe_allow_html=True)
 
 # Segunda Parte
